aff94f814ca17146d9cfe04e2383bc1236298f71
Badr <badr.sebaa@laplateforme.io>
2022-01-10 13:54:40 +0100

    db

--- blog.sql
+++ blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:8889
--- Généré le : lun. 10 jan. 2022 à 12:46
+-- Généré le : lun. 10 jan. 2022 à 12:54
 -- Version du serveur :  5.7.34
 -- Version de PHP : 7.4.21
 
