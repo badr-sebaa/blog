44d96e1eb2fc1da53fac84fcdb26210961d5e4d1
Badr <badr.sebaa@laplateforme.io>
2022-01-18 18:29:38 +0100

    afficher article brute

--- php/afficher-articles.php
+++ php/afficher-articles.php
@@ -9,6 +9,27 @@
   $req->execute(array($_SESSION['user']));
   $data = $req->fetch();
   
-   
 ?>
  
+
+
+        <!--SELECTION DE LA DB-->
+
+<?php
+
+// On récupere les données de l'utilisateur
+$req = $bdd->query('SELECT articles.article,utilisateurs.login,articles.date,articles.image FROM articles LEFT JOIN utilisateurs ON articles.id_utilisateur = utilisateurs.id');
+
+            while ($donnees = $req->fetch())
+            {
+                //On affiche les commentaires des utilisateurs 
+                echo "<ul><li class=\"him\">".$donnees['login']." LE ".$donnees['date']."</li><li class= \"me\">".$donnees['article']."</li>";
+                echo "<img src= \""."../articles/".$donnees['image']."\">";
+            }
+        ?>  
+
+
+
+
+
+ <?php include "footer.php"; ?>
---
+++ php/commentaire.php
@@ -0,0 +1,61 @@
+
+<?php
+session_start(); // Pour récupèrer nos données dans les variables : $_SESSION   
+require_once 'config.php'; // Pour faire la connexion à notre base de données
+?>
+
+
+
+<head>
+    <meta charset="utf-8">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+
+
+<body>
+  
+<header> <?php include "header.php";?> </header>
+
+
+<main>
+<section class="vh-100" style="background-color: #eee;">
+  <div class="container h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-lg-12 col-xl-11">
+        <div class="card text-black" style="border-radius: 25px;">
+          <div class="card-body p`-md-5">
+            <div class="row justify-content-center">
+              <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+
+                <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">COMMENTAIRE</p>
+
+                <form class="mx-1 mx-md-4" action="commentaire-traitement.php" method="post">
+
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <i !class="fas fa-key fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                      <input type="text" id="form3Example4cd" class="form-control" name="com"/>
+                      <label class="form-label" for="form3Example4cd">Message</label>
+                    </div>
+                  </div>
+
+                  <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                    <input type="submit" name="submit" class="btn btn-primary btn-lg" value="EXPRIME TOI !">
+                  </div>
+                  
+                </form>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+</main>
+
+<footer>
+<?php include "footer.php";?>
+</footer>
+
+</body>
